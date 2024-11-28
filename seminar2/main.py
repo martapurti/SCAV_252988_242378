840ed6c240aeac0ec3a6c025377f4954b3e46871
martapurti <marta.purti01@estudiant.upf.edu>
2024-11-28 18:45:24 +0100

    Update main.py

--- seminar2/main.py
+++ seminar2/main.py
@@ -18,13 +18,11 @@ app = FastAPI()
 
 # New functions ---------------------------------------------------------------------
 
-# TASK 1 
-
-#Create a new endpoint / feature which will let you to modify the resolution
+# TASK 1 : Create a new endpoint / feature which will let you to modify the resolution
 async def modify_res(input_video, output_video, width, height, quality):
     try:
         ffmpeg_command = [
-            "docker", "exec", "ffmpeg_container_s2",
+            "docker", "exec", "ffmpeg_container_s2", 
             "ffmpeg", "-y",
             "-i", f"/app/content/{input_video}",  
             "-vf", f"scale={width}:{height}", 
@@ -46,8 +44,7 @@ async def modify_res(input_video, output_video, width, height, quality):
         
 #-----------------------------------------------------------------------------
 
-# TASK 2 
-#2) Create a new endpoint / feature which will let you to modify the chroma subsampling
+# TASK 2 : Create a new endpoint / feature which will let you to modify the chroma subsampling
 #Practice of encoding images by implementing less resolution for Chroma / Luma
 
 def chroma_subsampling(input_video, output_video, subsampling_3ratio):
@@ -57,15 +54,18 @@ def chroma_subsampling(input_video, output_video, subsampling_3ratio):
             "docker", "exec", "ffmpeg_container_s2",
             "ffmpeg", "-y",
             "-i", f"/app/content/{input_video}",  
-            "-vf", f"format=yuv{subsampling_3ratio}p", #Chroma subsampling ratio = 4:2:2
+            #Apliquem un video filter per fer Chroma subsampling ratio = 4:2:2
+            "-vf", f"format=yuv{subsampling_3ratio}p", 
+            
             # Subsampling a:x:y (chroma resolution)
-            #a: horizontal sampling reference (4)
+            #a: horizontal sampling reference (usually 4)
             #x: num of chroma samples 1st row of pixels
             #y: num of changes of chroma samples bw 1st and 2nd rows of a pixel
-            "-c:v", "libx264",
-            "-b:v", "2M", 
-            "-pix_fmt", "yuv420p",
-            "-c:a", "aac", 
+            
+            "-c:v", "libx264", #Codificar amb el H.264 codec
+            "-b:v", "2M", #bitrate = 2Mbps
+            "-pix_fmt", "yuv420p", #per compatibilitat amb el nostre ordinador
+            "-c:a", "aac",  #codificar amb AAC codec
             f"/app/content/{output_video}"
         ]
 
@@ -79,17 +79,16 @@ def chroma_subsampling(input_video, output_video, subsampling_3ratio):
 
 #----------------------------------------------------------------------------
 
-# TASK 3 
-#Create a new endpoint/feature which lets you read the video info and print at least 5 relevant data from the video
+# TASK 3 : Create a new endpoint/feature which lets you read the video info and print at least 5 relevant data from the video
 def info_video(input_video):
     try:
         cmd = [
-            "docker", "exec", "ffmpeg_container_s2",
+            "docker", "exec", "ffmpeg_container_s2", 
             "ffprobe",
-            "-v", "error",
-            "-select_streams", "v:0",
-            "-show_entries", "stream=width,height,duration,bit_rate,codec_name,chroma_location",
-            "-of", "json",
+            "-v", "error", #only erors
+            "-select_streams", "v:0", #el primer video stream (v:0)
+            "-show_entries", "stream=width,height,duration,bit_rate,codec_name,chroma_location", #per extreure la informació
+            "-of", "json", #output json
             f"/app/content/{input_video}"
         ]
 
@@ -110,19 +109,19 @@ def info_video(input_video):
 
 #----------------------------------------------------------------------------
 
-# TASK 4 
+# TASK 4 : 
 #Cut BBB into 20 seconds only video
 #Export BBB(20s) audio as AAC mono track
 #Export BBB(20s) audio in MP3 stereo w/ lower bitrate
 #Export BBB(20s) audio in AC3 codec
-# TASK 4
+
 async def new_container(input_video, output_video):
     try:
         ffmpeg_cmd = [
             "docker", "exec", "ffmpeg_container_s2",
             "ffmpeg", "-y",
             "-i", f"/app/content/{input_video}",
-            "-t", "20",
+            "-t", "20", #retallar el video
             "-c:v", "copy",
             "-map", "0:v:0", "-map", "0:a:0", "-c:a:0", "aac", "-ac", "1", "-b:a:0", "128k", # ACC
             "-map", "0:a:0", "-c:a:1", "libmp3lame", "-b:a:1", "96k", "-ac:1", "2", # MP3
@@ -165,11 +164,9 @@ async def count_tracks(input_video):
         if process.returncode != 0:
             raise Exception(f"FFprobe error: {stderr.decode()}")
             
-        # Parse the FFprobe output
         metadata = json.loads(stdout.decode())
         streams = metadata.get("streams", [])
             
-        # Count track types
         track_counts = {"video": 0, "audio": 0, "subtitle": 0}
         for stream in streams:
             codec_type = stream.get("codec_type")
@@ -185,8 +182,7 @@ async def count_tracks(input_video):
 #----------------------------------------------------------------------------
 
 
-# TASK 6 
-# Create a new endpoint / feature which will output a video that will show the motion vectors
+# TASK 6 : Create a new endpoint / feature which will output a video that will show the motion vectors
 # Los macroblockes no se pueden hacer con ffmpeg
 async def motion_vectors(input_video, output_video):
     try:
@@ -216,8 +212,7 @@ async def motion_vectors(input_video, output_video):
 
 #----------------------------------------------------------------------------   
 
-# TASK 7 
-# Create a new endpoint / feature which will output a video that will show the YUV histogram
+# TASK 7 : Create a new endpoint / feature which will output a video that will show the YUV histogram
 async def yuv_histogram(input_video, output_video):
     try:
         ffmpeg_cmd = [
